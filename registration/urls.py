2e4cb78b0ab9348f8ff46cdb788631659a4986dd
stephanyanndelapena <sasdelapena.student@ua.edu.ph>
2025-10-08 11:26:55 +0800

    edit userlist page

--- registration/urls.py
+++ registration/urls.py
@@ -4,4 +4,5 @@ from .import views
 urlpatterns = [
     path('api/register/', views.register_user, name='register_user'),
     path('api/users/', views.list_user, name='list_user'),
+    path('api/users/<int:pk>/', views.user_detail, name='user_detail'),
 ]
